b5b3c4b9badb0415a889c00639f41a0916672978
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2023-12-11 19:32:10 +0100

    added batch capabilities

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from pico_csv_parser import pico_read_csv
 from oscilloscope_graphs import full_trace_1_channel
+import os
 
+INPUT_DIRECTORY = "input/"
+OUTPUT_DIRECTORY = "output/"
 
 if __name__ == "__main__":
-    parsed_data = pico_read_csv("exemples/20231211-0001_10.csv")
-
-    full_trace_1_channel(parsed_data, "Exemple de trace complète")
+    # fetch all the files in the input directory
+    input_files = os.listdir(INPUT_DIRECTORY)
+    for file in input_files:
+        # read the file
+        parsed_data = pico_read_csv(INPUT_DIRECTORY + file)
+        # plot the data
+        full_trace_1_channel(parsed_data, save_path=OUTPUT_DIRECTORY + file.replace(".csv", ".png"))
--- oscilloscope_graphs.py
+++ oscilloscope_graphs.py
@@ -1,19 +1,48 @@
 from matplotlib import pyplot as plt
 
-def full_trace_1_channel(parsed_data, title_text:str = "", is_digital=False, max_y=None, min_y=0) -> None:
+
+def full_trace_1_channel(parsed_data, title_text: str = None, is_digital=False, save_path=None, max_y=None,
+                         min_y=0, min_x=None, max_x=None) -> None:
+    """
+    Plots the full trace of a single channel
+    :param parsed_data:
+    :param title_text:
+    :param is_digital:
+    :param save_path:
+    :param max_y:
+    :param min_y:
+    :return:
+    """
     plt.plot(parsed_data[1][0], parsed_data[1][1])
 
     plt.xlabel(f"temps ({parsed_data[0][0]})")
     plt.ylabel(f"tension ({parsed_data[0][1]})")
 
-    plt.title(title_text)
+    if title_text is not None:
+        plt.title(title_text)
 
+    # setting the y axis limits
     if is_digital:
         plt.ylim(0, 5)
     else:
         if max_y is not None:
             plt.ylim(min_y, max_y)
         else:
-            plt.ylim(min_y, max(parsed_data[1][1]))
+            max_y = max(parsed_data[1][1])
+            if (max_y <= 1 and parsed_data[0][1]=="V") or (max_y <= 1000 and parsed_data[0][1]=="mV"):
+                plt.ylim(min_y, max_y)
+            else:
+                plt.ylim(0, 5)
 
-    plt.show()
+    # setting the x-axis limits
+    if min_x is None:
+        min_x = min(parsed_data[1][0])
+    if max_x is None:
+        max_x = max(parsed_data[1][0])
+    plt.xlim(min_x, max_x)
+
+    # saving of displaying the graph
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
