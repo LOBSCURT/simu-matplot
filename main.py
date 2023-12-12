094ce37fd441f7c057d30e537535fa8623740faa
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2023-12-12 18:12:22 +0100

    batch settings fully implemented, unit change not working for now

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pico_csv_parser import pico_read_csv
 from oscilloscope_graphs import draw_trace
 import os
+import shutil
 
 INPUT_DIRECTORY = "input/"
 OUTPUT_DIRECTORY = "output/"
 
-if __name__ == "__main__":
 
-
+def process_all_csv():
+    # check if the output directory exists if so clear it if not create it
+    if os.path.isdir(OUTPUT_DIRECTORY):
+        shutil.rmtree(OUTPUT_DIRECTORY)
+        os.mkdir(OUTPUT_DIRECTORY)
+    else:
+        os.mkdir(OUTPUT_DIRECTORY)
 
     # fetch all the files in the input directory
     input_section = os.listdir(INPUT_DIRECTORY)
@@ -17,14 +23,14 @@ if __name__ == "__main__":
             os.mkdir(OUTPUT_DIRECTORY + directory)
 
         input_setting_directory = os.listdir(INPUT_DIRECTORY + directory)
-        for setting in input_setting_directory:
-            setting_path = setting + "/"
+        for setting_folder in input_setting_directory:
+            setting_path = setting_folder + "/"
             if not os.path.isdir(OUTPUT_DIRECTORY + directory + setting_path):
                 os.mkdir(OUTPUT_DIRECTORY + directory + setting_path)
 
             input_files = os.listdir(INPUT_DIRECTORY + directory + setting_path)
 
-            if setting == "NOT_DEFINED":
+            if setting_folder == "NOT_DEFINED":
 
                 for file in input_files:
                     current_locale = directory + setting_path
@@ -34,4 +40,36 @@ if __name__ == "__main__":
                     draw_trace(parsed_data, save_path=OUTPUT_DIRECTORY + current_locale + file.replace(".csv", ".png"))
 
             else:
-                settings = setting.split("-")
+                settings = setting_folder.split(";") # format : title_title text;x_min_x_max_x;y_min_y_max_y;unit_force unit
+                y_lim = [None, None]
+                x_lim = [None, None]
+                title = ""
+                for setting in settings:
+                    setting_data = setting.split("_")
+                    if setting_data[0] == "title":
+                        title = setting_data[1]
+                    elif setting_data[0] == "x":
+                        x_lim = setting_data[1:]
+                        x_lim = list(map(lambda x: float(x.replace(",", ".")), x_lim))
+                    elif setting_data[0] == "y":
+                        y_lim = setting_data[1:]
+                        y_lim = list(map(lambda x: float(x.replace(",", ".")), y_lim))
+                    elif setting_data[0] == "unit":
+                        force_unit = setting_data[1]
+                    else:
+                        raise ValueError(f"Unknown setting : {setting_data[0]}  in folder {setting_folder}")
+
+                for file in input_files:
+                    current_locale = directory + setting_path
+                    # read the file
+                    parsed_data = pico_read_csv(INPUT_DIRECTORY + current_locale + file)
+                    # plot the data
+                    draw_trace(parsed_data, title_text=title,
+                               save_path=OUTPUT_DIRECTORY + current_locale + file.replace(".csv", ".png"),
+                               min_x=x_lim[0], max_x=x_lim[1], min_y=y_lim[0], max_y=y_lim[1])
+
+
+
+
+if __name__ == "__main__":
+    process_all_csv()
--- oscilloscope_graphs.py
+++ oscilloscope_graphs.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 
 def draw_trace(parsed_data, title_text: str = None, is_digital=False, save_path=None, max_y=None,
-               min_y=0, min_x=None, max_x=None) -> None:
+               min_y=0, min_x=None, max_x=None, force_unit=None) -> None:
     """
     Plots the full trace of a single channel
     :param parsed_data:
@@ -13,24 +13,53 @@ def draw_trace(parsed_data, title_text: str = None, is_digital=False, save_path=
     :param min_y:
     :return:
     """
-    plt.plot(parsed_data[1][0], parsed_data[1][1])
-    if parsed_data[1][2] != []:
-        plt.plot(parsed_data[1][0], parsed_data[1][2])
+    # change units if needed TODO : does not work right now
+    if force_unit is not None:
+        if force_unit == "V":
+            if parsed_data[0][1] == "mV":
+                parsed_data[1][1] = list(map(lambda x: x / 1000, parsed_data[1][1]))
+                try:
+                    parsed_data[1][2] = list(map(lambda x: x / 1000, parsed_data[1][2]))
+                except IndexError:
+                    pass
+                parsed_data[0][1] = "V"
+        elif force_unit == "mV":
+            if parsed_data[0][1] == "V":
+                parsed_data[1][1] = list(map(lambda x: x * 1000, parsed_data[1][1]))
+                try:
+                    parsed_data[1][2] = list(map(lambda x: x * 1000, parsed_data[1][2]))
+                except IndexError:
+                    pass
+                parsed_data[0][1] = "mV"
+        else:
+            raise ValueError(f"Unknown unit : {force_unit}")
 
-    plt.xlabel(f"temps ({parsed_data[0][0]})")
-    plt.ylabel(f"tension ({parsed_data[0][1]})")
+
+    plt.tick_params(axis='both', which='major', labelsize=14, direction="in")
+    plt.grid(True, which='major', axis='both', linestyle='--')
+
+    plt.plot(parsed_data[1][0], parsed_data[1][1], linewidth=1)
+    if parsed_data[1][2] == []:
+        pass
+    else:
+        plt.plot(parsed_data[1][0], parsed_data[1][2], linewidth=1)
+
+    plt.xlabel(f"temps ({parsed_data[0][0]})", fontsize=14)
+    plt.ylabel(f"tension ({parsed_data[0][1]})", fontsize=14)
 
     if title_text is not None:
         plt.title(title_text)
 
-    # setting the y axis limits
+    # setting the y-axis limits
     if is_digital:
         plt.ylim(0, 5.05)
     else:
+        if min_y is None:
+            min_y = 0
         if max_y is not None:
             plt.ylim(min_y, max_y)
         else:
-            max_y = max(parsed_data[1][1])
+            max_y = max(parsed_data[1][1]) + 0.05 * max(parsed_data[1][1])
             if (max_y <= 1 and parsed_data[0][1]=="V") or (max_y <= 1000 and parsed_data[0][1]=="mV"):
                 plt.ylim(min_y, max_y)
             else:
@@ -45,8 +74,8 @@ def draw_trace(parsed_data, title_text: str = None, is_digital=False, save_path=
 
     # saving of displaying the graph
     if save_path is not None:
-        plt.savefig(save_path)
+        plt.savefig(save_path, bbox_inches = 'tight' )
     else:
-        plt.show()
+        plt.show(bbox_inches = 'tight' )
 
     plt.close()
